d20499659b344aeeb52b46512093c3a05d78607e
DeepanshuSaxena14 <dsaxen14@asu.edu>
2026-04-04 15:10:30 -0700

    API created

---
+++ frontend/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppealPage from './src/pages/AppealPage';
+
+function App() {
+  return (
+    <div className="App">
+      <AppealPage />
+    </div>
+  );
+}
+
+export default App;
